f1b7c2957ff1f71093bcea9ebb97f85b2a306c61
Diogo Alves de Andrade <diogo10-1@hotmail.com>
2022-11-11 22:28:36 -0300

    fixed connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/users'),
+    MongooseModule.forRoot('mongodb://localhost:27017/admin'),
     UsersModule,
   ],
   controllers: [],
